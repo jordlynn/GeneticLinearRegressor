414a531fb62b40b26ea00b48def6a66fe0925581
Jordan <snake.river52@gmail.com>
2019-07-23 13:07:20 -0700

    Tuning GA parametres, need moar RAM

--- model.py
+++ model.py
@@ -7,16 +7,21 @@ print("done init")
 
 rng = check_random_state(0)
 
-est_gp = SymbolicRegressor(population_size=2000,
-                           generations=20, stopping_criteria=0.01,
-                           p_crossover=0.7, p_subtree_mutation=0.1,
-                           p_hoist_mutation=0.05, p_point_mutation=0.1,
-                           max_samples=0.9, verbose=1,
-                           parsimony_coefficient=0.01, random_state=0)
+est_gp = SymbolicRegressor(population_size=100,
+                           generations=20,
+                           p_crossover=0.7, 
+                           p_subtree_mutation=0.1,
+                           p_hoist_mutation=0.05, 
+                           p_point_mutation=0.1,
+                           max_samples=0.9, 
+                           n_jobs=-1, 
+                           verbose=1,
+                           parsimony_coefficient=0.01, 
+                           random_state=0, 
+                           function_set=('add', 'sub', 'mul', 'div', 'log', 'sqrt', 'sin'))
 
-est_gp.fit(loadedData.df[loadedData.feature_names], loadedData.df[loadedData.target_name])
+est_gp.fit(loadedData.df[loadedData.TrainingDataFeatureNames], loadedData.df[loadedData.target_name])
 print(est_gp._program)
-print(numerai_score(loadedData.df[loadedData.target_name], est_gp.))
 
 def numerai_score(y_true, y_pred):
      rank_pred = y_pred.groupby(eras).apply(lambda x: x.rank(pct=True, method="first"))
--- numeraidata.py
+++ numeraidata.py
@@ -14,9 +14,12 @@ class numeraiData():
         self.df = pandas.read_csv("data/numerai_training_data.csv").set_index("id")
         self.td = pandas.read_csv("data/numerai_tournament_data.csv").set_index("id")
 
-        self.feature_names = [f for f in self.df.columns if f.startswith("feature")]
+        self.TrainingDataFeatureNames = [f for f in self.df.columns if f.startswith("feature")]
+        self.TestDataFeatureNames = [f for f in self.td.columns if f.startswith("feature")]
+
         self.target_name = TARGET_NAME
-        print(f"Loaded {len(self.feature_names)} features")
+        print(f"Loaded {len(self.TrainingDataFeatureNames)} features from training data")
+        print(f"Loaded {len(self.TestDataFeatureNames)} features from test data")
 
         self.feature_groups = {
             g: [c for c in self.df if c.startswith(f"feature_{g}")]
